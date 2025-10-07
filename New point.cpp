116f9d17083f70a98f75f6217509c4263d9e4500
Daremyrtle <167142814+Daremyrtle@users.noreply.github.com>
2025-10-07 12:33:11 -0400

    Create New point.cpp

---
+++ New point.cpp
@@ -0,0 +1,30 @@
+#include "Point.h"
+#include <iostream>
+using namespace std;
+
+Point::Point() {
+	x = y = 0;
+}
+void Point::setValues(double xVal, double yVal) {
+	x[i] = xVal;
+	y[i] = yVal;
+}
+double Point::getXvalue() {
+	return x;
+}
+double Point::getYvalue() {
+	return y;
+}
+void Point::display() {
+	cout << "(" << x << "," << y << ")" << endl;
+}
+double Point::dist(Point p) {
+	return sqrt((abs(pow(x - p.x, 2)) + (abs(pow(y - p.y, 2)))));
+}
+double Point::h(Point p) {
+	return h;
+}
+ostream& operator<<(ostream& out, Point& p) {
+	return out << "(" << p.getXvalue() << "," << p.getYvalue() << ")";
+	
+}
